e74da7f96c51afcb874d144949b846b844d8dca4
Michael Harris <mharris@luabase.com>
2024-02-24 13:55:51 -0500

    change function signature for easier debugging

--- computer-systems/introduction-to-c/hashmap/hashmap.c
+++ computer-systems/introduction-to-c/hashmap/hashmap.c
@@ -28,30 +28,30 @@ Hashmap *Hashmap_new() {
   return hm;
 }
 
-void Hashmap_free(Hashmap *hm) {
-  for (int i; i < hm->buckets; i++) {
-    if (hm->maps[i] != NULL) {
-      LinkMap_free(hm->maps[i]);
+void Hashmap_free(Hashmap *h) {
+  for (int i; i < h->buckets; i++) {
+    if (h->maps[i] != NULL) {
+      LinkMap_free(h->maps[i]);
     }
   }
-  free(hm);
+  free(h);
 }
 
-void Hashmap_set(Hashmap *hm, char *k, void *v) {
-  int i = hash(k, hm->buckets);
-  LinkMap *lm = hm->maps[i];
+void Hashmap_set(Hashmap *h, char *k, void *v) {
+  int i = hash(k, h->buckets);
+  LinkMap *lm = h->maps[i];
   if (lm == NULL) {
     lm = LinkMap_new();
     lm->head = Node_new(k, v);
-    hm->maps[i] = lm;
+    h->maps[i] = lm;
   } else {
     LinkMap_set(lm, k, v);
   }
 }
 
-void *Hashmap_get(Hashmap *hm, char *k) {
-  int i = hash(k, hm->buckets);
-  LinkMap *lm = hm->maps[i];
+void *Hashmap_get(Hashmap *h, char *k) {
+  int i = hash(k, h->buckets);
+  LinkMap *lm = h->maps[i];
   if (lm != NULL) {
     Node *n = LinkMap_get(lm, k);
     if (n != NULL) {
@@ -61,9 +61,9 @@ void *Hashmap_get(Hashmap *hm, char *k) {
   return NULL;
 }
 
-void Hashmap_delete(Hashmap *hm, char *k) {
-  int i = hash(k, hm->buckets);
-  LinkMap *lm = hm->maps[i];
+void Hashmap_delete(Hashmap *h, char *k) {
+  int i = hash(k, h->buckets);
+  LinkMap *lm = h->maps[i];
   if (lm != NULL) {
     LinkMap_delete(lm, k);
   }
