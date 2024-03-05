af462dd2656d106343bd10aa6752eca797f535b1
Michael Harris <mharris@luabase.com>
2024-03-04 21:26:53 -0500

    comment code

--- computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
+++ computer-systems/low-level-programming/sum-to-n/sum_to_n.asm
@@ -1,9 +1,12 @@
+; formula used is n(n + 1) / 2
+; source https://math.stackexchange.com/a/60581
+
         global sum_to_n
         section .text
 sum_to_n:
-        mov rax, rdi
-        mov rdx, rax
-        mul rax
-        add rax, rdi
-        shr rax, 1
+        mov rax, rdi                   ; copy n to rax (also avoids needing to clear rax)
+        mov rdx, rax                   ; copy n to rdx
+        mul rax                        ; n = n^2
+        add rax, rdi                   ; n = n + 1
+        shr rax, 1                     ; n = n / 2
         ret
