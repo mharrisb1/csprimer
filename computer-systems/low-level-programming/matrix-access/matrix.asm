6ef319e151c3c06a24cac86e411adeb0b63790b6
Michael Harris <mharris@luabase.com>
2024-03-07 13:22:12 -0500

    add initial submission

--- computer-systems/low-level-programming/matrix-access/matrix.asm
+++ computer-systems/low-level-programming/matrix-access/matrix.asm
@@ -1,9 +1,14 @@
-section .text
-global index
-index:
-	; rdi: matrix
-	; esi: rows
-	; edx: cols
-	; ecx: rindex
-	; r8d: cindex
-	ret
+        section .text
+        global mat_index
+mat_index:
+; rdi: matrix
+; esi: rows
+; edx: cols
+; ecx: rindex
+; r8d: cindex
+        imul rdx, 4                    ; calc row width
+        imul rcx, rdx                  ; calc row offset
+        imul r8, 4                     ; calc col offset
+        add rcx, r8                    ; calc total offset
+        mov rax, [rdi + rcx]           ; dereference element at offset
+        ret
