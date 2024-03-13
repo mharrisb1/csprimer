5044c863d099b4d331dff6334c467b6d7e604cc9
Michael Harris <mharris@luabase.com>
2024-03-12 21:08:20 -0400

    follow along with Oz's solution

--- computer-systems/low-level-programming/x86-64-pangram/pangram.asm
+++ computer-systems/low-level-programming/x86-64-pangram/pangram.asm
@@ -1,38 +1,24 @@
+        %define MASK 07fffffeh
+
         section .text
         global pangram
 pangram:
 ; rdi: source string
-        xor rax, rax                   ; counter
-        xor rsi, rsi                   ; address offset
-loop:
-        xor rdx, rdx                   ; consumed char
-        mov dl, byte [rdi + rsi*1]     ; consume next char
-        add rsi, 1                     ; offset++
-        cmp dl, 0                      ; handle end of string
-        je compare_bitvec
-        or rdx, 020h                   ; flip on the 6h bit to force lowercase
-        cmp dl, 60                     ; check is character is lowercase alpha
-        jg bit_pack
-        jmp loop
+        xor ecx, ecx                   ; bs = 0
 
-bit_pack:
-        sub rdx, 061h                  ; convert to ith lowercase letter of sequence (i.e. a->1)
-        mov rcx, rdx                   ; move that value to shift count register
-        mov rdx, 1                     ; used for 1 << n
-        shl rdx, cl                    ; 1 << n
-        or rax, rdx                    ; pack to bit vec
-        jmp loop
+.loop:
+        movzx edx, byte[rdi]           ; c = *s
+        cmp edx, 0
+        je .end
+        add rdi, 1                     ; s++
+        cmp edx, '@'                   ; if c is first 64 chars of ascii table
+        jl .loop                       ; continue
+        bts ecx, edx
+        jmp .loop
 
-compare_bitvec:
-        and rax, 03FFFFFFh             ; apply bitmask
-        cmp rax, 03FFFFFFh             ; check if result equals bitmask
-        jne false
-        mov rax, 1
-        jmp done
-
-false:
-        mov rax, 0
-        jmp done
-
-done:
+.end:
+        xor eax, eax
+        and ecx, MASK
+        cmp ecx, MASK
+        sete al
         ret
