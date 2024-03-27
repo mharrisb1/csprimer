562f6df1fb209faaf49cd67ff93c121e84eb7b7d
Michael Harris <mharris@luabase.com>
2024-03-26 23:07:53 -0400

    follow along with Oz's solution

--- computer-systems/low-level-programming/x86-fib/fib.asm
+++ computer-systems/low-level-programming/x86-fib/fib.asm
@@ -1,21 +1,20 @@
         section .text
         global fib
-; derived from "Recursion" example from https://cs.lmu.edu/~ray/notes/nasmtutorial/
 fib:
-        xor rax, rax                   ; zero out return value (r)
-        jmp .l1
+        mov rax, rdi                   ; if n <= 1 return n
+        cmp rdi, 1
+        jle .end
 
-.l1:
-        cmp rdi, 1                     ; check n <= 1
-        jnbe .l2                       ; jump to next layer if not
-        add rax, rdi                   ; r += n
-        ret
+        sub rdi, 1
+        push rdi                       ; push n - 1 to top of stack
+        call fib                       ; f(n - 1)
+        pop rdi
 
-.l2:
-        push rdi                       ; save n to stack
-        sub rdi, 1                     ; n--
-        call .l1                       ; call previous layer
-        sub rdi, 1                     ; n--
-        call .l1                       ; call previous layer
-        pop rdi                        ; restore n
+        push rax                       ; push f(n - 1) to the stack
+        sub rdi, 1
+        call fib                       ; f(n - 2)
+
+        pop rcx                        ; pop f(n - 1) from top of stack
+        add rax, rcx                   ; f(n - 1) + f(n - 2)
+.end:
         ret
