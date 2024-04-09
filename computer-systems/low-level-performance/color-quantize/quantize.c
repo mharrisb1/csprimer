a73342fa1cfbaa56952544a6c151378f20f139a3
Michael Harris <mharris@luabase.com>
2024-04-08 21:37:21 -0400

    submit one-liner solution

--- computer-systems/low-level-performance/color-quantize/quantize.c
+++ computer-systems/low-level-performance/color-quantize/quantize.c
@@ -1,13 +1,6 @@
-#define RED_MASK 0xE0
-#define GREEN_MASK 0x1C
-#define BLUE_MASK 0x03
+#define MASK 0xE0
 
 unsigned char quantize(unsigned char red, unsigned char green,
                        unsigned char blue) {
-  unsigned char out = 0;
-  out += red & RED_MASK;
-  out += (green >> 3) & GREEN_MASK;
-  out += (blue >> 6) & BLUE_MASK;
-
-  return out;
+  return (red & MASK) + (green & MASK >> 3) + (blue & MASK >> 6);
 }
