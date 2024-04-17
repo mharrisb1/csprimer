293a8c3fc98a4b948d65e2e18b23fafe812ea929
Michael Harris <mharris@luabase.com>
2024-04-16 22:25:12 -0400

    add second refactor with only single iteration of lines

--- computer-systems/memory-hierarchy/pointer-chase/metrics.py
+++ computer-systems/memory-hierarchy/pointer-chase/metrics.py
@@ -1,70 +1,31 @@
 import csv
-import datetime
 import math
 import time
 
-
-class Users:
-    def __init__(
-        self,
-        ids: list[int],
-        names: list[str],
-        ages: list[int],
-    ) -> None:
-        self.ids = ids
-        self.names = names
-        self.ages = ages
-
-
-class Payments:
-    def __init__(
-        self,
-        user_ids: list[int],
-        amounts: list[float],
-        times: list[datetime.datetime],
-    ) -> None:
-        self.user_ids = user_ids
-        self.amounts = amounts
-        self.times = times
-
-
-def average_age(ages: list[int]):
-    return sum(ages) / len(ages)
-
-
-def average_payment_amount(amounts: list[float]):
-    return sum(amounts) / len(amounts)
-
-
-def stddev_payment_amount(amounts: list[float]):
-    mean = average_payment_amount(amounts)
-    n = len(amounts)
-    squared_diffs = sum([(x - mean) ** 2 for x in amounts])
-    return math.sqrt(squared_diffs / n)
-
-
-def load_data():
-    users = {}
-    with open("users.csv") as f:
-        test = [list(col) for col in zip(*csv.reader(f))]
-        ids, names, ages, *_ = test
-        users = Users([int(id) for id in ids], names, [int(age) for age in ages])
-    with open("payments.csv") as f:
-        amounts, ts, uids = [list(col) for col in zip(*csv.reader(f))]
-        payments = Payments(
-            [int(id) for id in uids],
-            [int(amount) * 1e-2 for amount in amounts],
-            [datetime.datetime.fromisoformat(t) for t in ts],
-        )
-    return users, payments
-
-
 if __name__ == "__main__":
     t = time.perf_counter()
-    users, payments = load_data()
+
+    sum_age, n_users = 0, 0
+    with open("users.csv") as f:
+        for line in csv.reader(f):
+            sum_age += int(line[2])
+            n_users += 1
+
+    sum_amt, sq_sum_amt, n_pmts = 0, 0, 0
+    with open("payments.csv") as f:
+        for line in csv.reader(f):
+            amount = int(line[0]) * 1e-2
+            sum_amt += amount
+            sq_sum_amt += amount * amount
+            n_pmts += 1
+
     print(f"Data loading: {time.perf_counter() - t:.3f}s")
+
     t = time.perf_counter()
-    assert abs(average_age(users.ages) - 59.626) < 0.01
-    assert abs(stddev_payment_amount(payments.amounts) - 288684.849) < 0.01
-    assert abs(average_payment_amount(payments.amounts) - 499850.559) < 0.01
-    print(f"Computation {time.perf_counter() - t:.3f}s")
+    assert abs((sum_age / n_users) - 59.626) < 0.01
+    assert (
+        abs(math.sqrt((sq_sum_amt / n_pmts) - (sum_amt / n_pmts) ** 2) - 288684.849)
+        < 0.01
+    )
+    assert abs((sum_amt / n_pmts) - 499850.559) < 0.01
+    print(f"Computation {time.perf_counter() - t:.6f}s")
