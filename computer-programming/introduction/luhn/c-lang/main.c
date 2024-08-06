bd42ef909335f5363b3d1cca811d8c6467db5a34
Michael Harris <mharris@luabase.com>
2024-08-05 22:51:35 -0400

    refactor c solution

--- computer-programming/introduction/luhn/c-lang/main.c
+++ computer-programming/introduction/luhn/c-lang/main.c
@@ -1,45 +1,27 @@
 #include <assert.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
-static const int table[10] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
-
-void reverse(char *s, int len) {
-  int left  = 0;
-  int right = len - 1;
-  while (left < right) {
-    char temp = s[left];
-    s[left]   = s[right];
-    s[right]  = temp;
-    left++;
-    right--;
-  }
-}
-
-int ctoi(char c) { return c ^ 0x30; }
+static const uint8_t table[10] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
 
 int is_valid(char *digits) {
-  int n   = 0;
-  int len = strlen(digits);
+  uint8_t b, c;
+  uint8_t i, k;
+  uint16_t n = 0;
 
-  char *s = malloc((len + 1) * sizeof(char));
-  strcpy(s, digits);
-  reverse(s, len);
+  i = k = strlen(digits) - 1;
+  c     = digits[i] ^ 0x30;
 
-  int check = ctoi(s[0]);
-
-  for (int i = 1; i < len; i++) {
-    int t = ctoi(s[i]);
-    n += i % 2 == 0 ? t : table[t];
+  while (i--) {
+    b = digits[i] ^ 0x30;
+    n += (k - i) % 2 == 1 ? table[b] : b;
   }
 
-  free(s);
-
-  return check == 10 - (n % 10);
+  return c == 10 - (n % 10);
 }
 
 int main() {
-  // valid credit card numbers
+  // Valid credit card numbers
   assert(is_valid("4532015112830366") == 1);
   assert(is_valid("4916239364668151") == 1);
   assert(is_valid("6011514433546201") == 1);
@@ -50,4 +32,15 @@ int main() {
   assert(is_valid("4916239364668159") == 0);
   assert(is_valid("6011514433546202") == 0);
   assert(is_valid("379354508162307") == 0);
+
+  // Edge cases: valid numbers with varying lengths
+  assert(is_valid("79927398713") == 1); // 11 digits
+
+  // Edge cases: invalid numbers with varying lengths
+  assert(is_valid("1234567812345678") == 0); // 16 digits
+  assert(is_valid("79927398710") == 0);      // 11 digits
+
+  // Additional edge cases
+  assert(is_valid("0000000000000000") == 0); // All zeroes
+  assert(is_valid("4111111111111111") == 1); // Common test number
 }
