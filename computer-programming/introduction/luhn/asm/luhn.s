29b5719284fdbafdcabf968220ab27175ca0eb72
Michael Harris <mharris@luabase.com>
2024-08-06 22:46:54 -0400

    add asm solutionn (not compiling)

--- computer-programming/introduction/luhn/asm/luhn.s
+++ computer-programming/introduction/luhn/asm/luhn.s
@@ -1,67 +1,72 @@
 /*
- *        @author Michael Harris
- *        @date 2024-07-31
- *
  *        Validates a 16 character sequence of digits using Luhn algorithm
  *
  *        int is_valid(char digits[16]);
  *
- *        @param digits - Digits sequence
-*/
+ *        @param digits - 16-byte digits sequence
+ *
+ *        Register glossary
+ *        -----------------
+ *        X1 - index (i)
+ *        X2 - accumulator (k)
+ *        X3 - factor (f)
+ *        X4 - digits number (n)
+ *        X5 - lookup address (*l)
+ */
 
-.data
-        table: .word 0, 2, 4, 6, 8, 1, 3, 5, 7, 9
+.section .data,"aw"
+.align 2
+lookup:
+    .byte 0x00
+    .byte 0x02
+    .byte 0x04
+    .byte 0x06
+    .byte 0x08
+    .byte 0x01
+    .byte 0x03
+    .byte 0x05
+    .byte 0x07
+    .byte 0x09
+    .byte 0x00
+    .byte 0x01
+    .byte 0x02
+    .byte 0x03
+    .byte 0x04
+    .byte 0x05
+    .byte 0x06
+    .byte 0x07
+    .byte 0x08
+    .byte 0x09
 
-.text
-        .global _is_valid
+.section .text,"ax"
+.global _is_valid
 
 _is_valid:
-        mov w2, wzr                // n
-        mov w3, wzr                // i
-        mov w4, wzr                // c
+    mov w1, wzr                // i
+    mov w2, wzr                // k
+    mov w3, wzr                // f
+    adrp x5, lookup            // Load the page address of lookup into x5
+    add x5, x5, :lo12:lookup   // Add the page offset of lookup to x5
+    mov w6, 0x0A               // static mask
+    mov w7, 0x30               // static mask
 
 .loop:
-        ldr w5, [x1, x3]
-        eor w5, w5, 0x30
-        add w3, w3, 1
-        cmp w3, 15
-        b.eq .end
+    cmp w1, 0x10               // i == 16
+    b.eq .mod10
+    ldrb w4, [x0, w1, uxtw]    // n = digits[i]
+    eor w4, w4, w7             // n = uint(n)
+    add w4, w4, w3             // n += f
+    ldrb w4, [x5, w4, uxtw]    // n = lookup[n]
+    add w2, w2, w4             // k += n
+    eor w3, w3, w6             // f ^= 0x0A
+    add w1, w1, 1              // i++
+    b .loop
 
-.o1:
-        
-
-.o2:
+.mod10:
+    udiv w0, w2, w6
+    mul w0, w0, w6
+    sub w0, w2, w0
 
 .end:
-        
-
-_is_valid:
-        sub sp, sp, 16               // allocate space on stack
-
-        /* @dev first 20 bytes are reserved for copy buffer */
-        str wzr, [sp, 20]            // N (accumulator)
-        str wzr, [sp, 24]            // K (len of digits)
-        str wzr, [sp, 28]            // C (check digit)
-
-/**/
-loop1:
-        ldr w1, [sp, 24]             // K
-        ldr w2, [x0, x1]             // D[K]
-        str w2, [sp, x1]             // SP[K] = D[K]
-        str w2, [sp, 28]             // C = D[K]
-        cmp w2, wzr                  // D[K] == 0
-        b.eq loop2
-        add w1, w1, 1                // K++
-        str w1, [sp, 24]             // K
-        mov w3, w1                   // copy K
-        b loop1
-
-loop2:
-        sub w1, w1, 1                // K--
-        cmp w1, wzr                  // K == 0
-        b.eq end
-        sub w2, w3, w1               // copy K - K
-
-end:
-        add sp, sp, 32               // deallocate space on stack
-        ret
+    eor w0, w0, w6
+    ret
