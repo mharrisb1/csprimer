4bf5b4959a644ef130d90452411fe95d8bbfab4b
Michael Harris <mharris@definite.app>
2025-06-27 10:31:13 -0600

    add fork and execvp

--- operating-systems/programs-and-processes/shell/main.c
+++ operating-systems/programs-and-processes/shell/main.c
@@ -1,6 +1,8 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAXARG 256
 #define MAXBUF 4096
@@ -8,11 +10,18 @@
 static const char* const PROMPT = "> ";
 static const char* const DELIM  = " \t\n";
 
-static int  tokenize(char *input, char *argv[], size_t *argc);
 static void print_help();
+static void handle_sigint(int sig);
+static int  tokenize(char *input, char *argv[], size_t *argc);
+
+volatile pid_t childpid = 0;
 
 int main() {
 	char buf[MAXBUF];
+	size_t argc;
+	char *argv[MAXARG];
+
+	signal(SIGINT, handle_sigint);
 
 	for (;;) {
 		printf(PROMPT);
@@ -21,25 +30,47 @@ int main() {
 			if (feof(stdin) != 0) exit(0);
 			if (ferror(stdin) != 0) {
 				perror("fgets error");
-				exit(1);
+				exit(EXIT_FAILURE);
 			}
 		}
 
-		size_t argc = 0;
-		char *argv[MAXARG];
+		// tokenization
+		if (tokenize(buf, argv, &argc) != 0) exit(EXIT_FAILURE);
 
-		if (tokenize(buf, argv, &argc) != 0) exit(1);
+		// eval
 		if (argc == 0) 			     continue;
-		if (strcmp(argv[0], "quit") == 0)    exit(0);
-		if (strcmp(argv[0], "help") == 0)    print_help();
+		if (strcmp(argv[0], "quit") == 0)    exit(EXIT_SUCCESS);
+		if (strcmp(argv[0], "help") == 0)    { print_help(); continue; }
 
-		printf("argc = %zu\n", argc);
-		for (size_t i = 0; i < argc; i++) {
-			printf("argv[%zu] = '%s'\n", i, argv[i]);
+		if ((childpid = fork()) < 0) {
+			perror("fork error");
+			exit(EXIT_FAILURE);
 		}
+
+		// child process
+		if (childpid == 0) {
+			if (execvp(argv[0], argv) < 0) {
+				perror("exec error");
+				exit(EXIT_FAILURE);
+			}
+			exit(EXIT_FAILURE);
+		}
+
+		// parent
+		int status;
+		waitpid(childpid, &status, 0);
 	}
 	
-	return 0;
+	return EXIT_SUCCESS;
+}
+
+static void print_help() { printf("Builtins are `quit` and `help`\n"); }
+
+static void handle_sigint(int sig) {
+	if (!childpid) return;
+	if (kill(childpid, sig) < 0) {
+		perror("SIGINT error");
+	}
 }
 
 static int tokenize(char *input, char *argv[], size_t *argc) {
@@ -57,9 +88,7 @@ static int tokenize(char *input, char *argv[], size_t *argc) {
 			return -1;
 		}
 	}
+	argv[*argc] = NULL;
 	return 0;
 }
 
-static void print_help() {
-	printf("Builtins are `quit` and `help`\n");
-}
