142a9292026de3e9997b47a049716563965a3c9b
Michael Harris <mharris@definite.app>
2025-09-29 12:46:31 -0600

    checkpoint

--- operating-systems/programs-and-processes/shell/src/readline.c
+++ operating-systems/programs-and-processes/shell/src/readline.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,33 +10,64 @@
 
 #define MAXBUF 256
 
+#define NUL 0x00
+#define ETX 0x03
+#define EOT 0x04
+#define LF  0x0A
+#define CR  0x0D
+#define ESC 0x1B
+#define NAK 0x15
+#define DEL 0x7F
+
+#define CRLF ((char[]){CR, LF})
+
+enum vt_sequence_t {
+  VT_HOME = 1000,
+  VT_INSERT,
+  VT_DELETE,
+  VT_END,
+  VT_PGUP,
+  VT_PGDN
+};
+
+enum xterm_sequence_t {
+  XTERM_UP = 2000,
+  XTERM_DOWN,
+  XTERM_RIGHT,
+  XTERM_LEFT,
+  XTERM_END,
+  XTERM_HOME
+};
+
 static struct termios orig;
 static void           setup();
 static void           teardown();
+static int            read_keypress(const int fd, char buf[4], ssize_t *n);
 
 char *readline(const char *prompt) {
   setup();
 
-  char  *buf = malloc(MAXBUF);
-  size_t len = MAXBUF;
-  size_t pos = 0;
+  char   *buf = malloc(MAXBUF);
+  size_t  len = MAXBUF, pos = 0;
+  ssize_t n;
+  char    seq[4];
 
   if (!buf) {
     teardown();
+    perror("Failed to allocate buffer");
     return NULL;
   }
 
   write(STDOUT_FILENO, prompt, strlen(prompt));
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1) {
-    teardown();
-    break;
-  }
+  int key;
+  while ((key = read_keypress(STDIN_FILENO, seq, &n)) != CR) {}
 
-  buf[pos] = '\0';
+  write(STDOUT_FILENO, CRLF, 2);
+
+  buf      = realloc(buf, len + 1);
+  buf[len] = '\0';
   teardown();
-
   return buf;
 }
 
@@ -71,4 +103,45 @@ static void teardown() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig) == -1) {
     perror("Failed to disable raw mode");
   }
+}
+
+static int read_keypress(const int fd, char buf[4], ssize_t *n) {
+  *n = read(fd, buf, 4);
+
+  if (*n < 1 || buf[0] == EOT) return EOF;
+  if (buf[0] != ESC || *n == 1) return buf[0];
+
+  // ANSI Escape Codes (https://en.wikipedia.org/wiki/ANSI_escape_code)
+  if (buf[1] != '[' || *n == 2) return buf[0]; // Non-CSI
+
+  // Terminal input sequences
+  // (https://en.wikipedia.org/wiki/ANSI_escape_code#Terminal_input_sequences)
+
+  // Assume xterm (subset only)
+  if (*n == 3) {
+    switch (buf[2]) {
+      case 'A': return XTERM_UP;
+      case 'B': return XTERM_DOWN;
+      case 'C': return XTERM_RIGHT;
+      case 'D': return XTERM_LEFT;
+      case 'F': return XTERM_END;
+      case 'H': return XTERM_HOME;
+    }
+  }
+  // Assume vt (subset only)
+  else if (buf[3] == '~') {
+    switch (buf[2]) {
+      case '1': return VT_HOME;
+      case '2': return VT_INSERT;
+      case '3': return VT_DELETE;
+      case '4': return VT_END;
+      case '5': return VT_PGUP;
+      case '6': return VT_PGDN;
+      case '7': return VT_HOME;
+      case '8': return VT_END;
+    }
+  }
+
+  // default
+  return ESC;
 }
