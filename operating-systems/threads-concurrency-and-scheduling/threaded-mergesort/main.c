d049137614c63867c4feb97cea83fefe17e59ff2
Michael Harris <mharris@definite.app>
2025-12-01 22:43:38 -0700

    use naive threading approach

--- operating-systems/threads-concurrency-and-scheduling/threaded-mergesort/main.c
+++ operating-systems/threads-concurrency-and-scheduling/threaded-mergesort/main.c
@@ -1,5 +1,11 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct {
+  int *arr;
+  int  n;
+} sortargs_t;
 
 void merge(int *arr, int n, int mid) {
   int i;
@@ -12,9 +18,9 @@ void merge(int *arr, int n, int mid) {
   for (i = 0; i < n; i++) {
     if (right == n) {
       x[i] = arr[left++];
-    } else if (left == mid || arr[right] <= arr[left]) {
+    } else if (left == mid) {
       x[i] = arr[right++];
-    } else if (arr[right] <= arr[left]) {
+    } else if (arr[right] < arr[left]) {
       x[i] = arr[right++];
     } else {
       x[i] = arr[left++];
@@ -29,12 +35,32 @@ void merge(int *arr, int n, int mid) {
   free(x);
 }
 
+void sort(int *arr, int n);
+
+void *start(void *args) {
+  int *arr = ((sortargs_t *)args)->arr;
+  int  n   = ((sortargs_t *)args)->n;
+  sort(arr, n);
+  return NULL;
+}
+
 void sort(int *arr, int n) {
   if (n < 2) return;
 
   int mid = n / 2;
-  sort(arr, mid);
+
+  // child execute left-hand side
+  pthread_t  t;
+  sortargs_t args = {arr, mid};
+  pthread_create(&t, NULL, start, (void *)&args);
+
+  // parent execute right-hand side
   sort(arr + mid, n - mid);
+
+  // wait for child
+  pthread_join(t, NULL);
+
+  // merge
   merge(arr, n, mid);
 }
 
